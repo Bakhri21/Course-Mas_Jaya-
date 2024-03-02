9ddb4cb958313854db6b49cb152206ced75e9b18
Bakhri21 <rayhannurahman21@gmail.com>
2024-03-02 10:09:37 +0700

    second commit

--- Pertemuan1/login.php
+++ Pertemuan1/login.php
@@ -10,10 +10,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $userinput = mysqli_real_escape_string($conn, htmlspecialchars($_POST["username"]));
     $userpassword = mysqli_real_escape_string($conn, htmlspecialchars($_POST["password"]));
 
-    if($userinput == $validuser){
-        if(){
-            
+    if ($userinput == $validuser) {
+        if ($userpassword == $validpassword) {
+?>
+            <script>
+                alert("Login Berhasil");
+                window.location.href = "index.html";
+            </script>
+        <?php
+        } else {
+        ?>
+            <script>
+                alert("Login Gagal Password anda salah");
+                window.location.href = "login.html";
+            </script>
+        <?php
         }
+    } else {
+        ?>
+        <script>
+            alert("Login Gagal Username anda salah");
+            window.location.href = "login.html";
+        </script>
+<?php
     }
-
 }
